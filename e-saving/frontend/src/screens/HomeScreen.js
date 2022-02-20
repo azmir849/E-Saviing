5b7421f0f7afb8c45d88fb9346c7f18ecf5c4aae
azmir849 <azmir15-849@diu.edu.bd>
2022-02-21 02:02:07 +0600

    Dynamic category and feature products done

--- e-saving/frontend/src/screens/HomeScreen.js
+++ e-saving/frontend/src/screens/HomeScreen.js
@@ -4,8 +4,9 @@ import Product from "../components/Product";
 import LoadingBox from "../components/LoadingBox";
 import MessageBox from "../components/MessageBox";
 import { useDispatch, useSelector } from "react-redux";
-import { listProducts } from "../actions/productActions";
+import { listProductCategories, listProducts } from "../actions/productActions";
 import HomeCarouselScreen from "./HomeCarouselScreen";
+import { Link } from "react-router-dom";
 
 export default function HomeScreen() {
   const dispatch = useDispatch();
@@ -15,21 +16,53 @@ export default function HomeScreen() {
   useEffect(() => {
     dispatch(listProducts({}));
   }, [dispatch]);
+
+  const productCategoryList = useSelector((state) => state.productCategoryList);
+  const {
+    loading: loadingCategories,
+    error: errorCategories,
+    categories,
+  } = productCategoryList;
+  useEffect(() => {
+    dispatch(listProductCategories());
+  }, [dispatch]);
+
   return (
-    <div>
+    <div className=''>
       <h2>Product category and feature products</h2>
       <HomeCarouselScreen />
-      {loading ? (
-        <LoadingBox></LoadingBox>
-      ) : error ? (
-        <MessageBox variant='danger'>{error}</MessageBox>
-      ) : (
-        <div className='row center'>
-          {products.map((product) => (
-            <Product key={product._id} product={product}></Product>
-          ))}
+
+      <div className='row categoryClass'>
+        <div className='col-2'>
+          <h2>Categories</h2>
+          {loadingCategories ? (
+            <LoadingBox></LoadingBox>
+          ) : errorCategories ? (
+            <MessageBox variant='danger'>{errorCategories}</MessageBox>
+          ) : (
+            categories.map((c) => (
+              <li key={c}>
+                <Link to={`/search/category/${c}`}>{c}</Link>
+              </li>
+            ))
+          )}
         </div>
-      )}
+
+        <div className='col-10'>
+          <h2 className='text-center'>Feature Products</h2>
+          {loading ? (
+            <LoadingBox></LoadingBox>
+          ) : error ? (
+            <MessageBox variant='danger'>{error}</MessageBox>
+          ) : (
+            <div className='row center'>
+              {products.map((product) => (
+                <Product key={product._id} product={product}></Product>
+              ))}
+            </div>
+          )}
+        </div>
+      </div>
     </div>
   );
 }
